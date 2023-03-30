764747765c2e0554aea99284d07b8f429a215622
Gilles De Kerf <Gilles.De.kerf@student.ehb.be>
2023-03-30 14:47:10 +0200

    rects en circls werkc. 6

--- js/index.js
+++ js/index.js
@@ -1,22 +1,41 @@
 "use strict";
-//import Figure from './Figure.js';
+import Figure from './Figure.js';
 //import Circle from './Circle.js';
-//import Rectangle from './Rectangle.js';
+import Rectangle from './Rectangle.js';
 //import Square from './Square.js';
 
 const form = {
     changeSelect(e) {
-
+        console.log('option picked by user');
+        const inputElement = `<label>
+        YOUR LABEL NAME
+        <input id="YOUR INPUT ID" type="number">
+      </label>
+      `
+        document.querySelector('#otherInputs').insertAdjacentHTML = ('beforeend', inputElement);
     },
     init() {
         console.log('Form init!');
+
+        document.querySelector('#form').addEventListener("submit", this.submitForm);
+        document.querySelector('#select').addEventListener("select", this.changeSelect());
     },
     inputElementsWithLabel(label, id) {
 
     },
     submitForm(e) {
+        e.preventDefault();
+        console.log('form submitted');
+
+        document.querySelector('#radius')
+        const rectangle = new Rectangle("Rectangle", 20, 20);
+        console.log(rectangle.innerHtml);
+        document.querySelector('#results').innerHTML = rectangle.innerHtml;
 
     }
 };
+
+
+
 
 form.init();
